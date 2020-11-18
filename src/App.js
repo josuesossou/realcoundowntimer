8f9f558378e277fdc716c01fe85a42fd4375d919
Josue <josuesossou8@gmail.com>
2020-11-18 13:35:13 -0500

    countdown page Url needs to be made public

--- src/App.js
+++ src/App.js
@@ -3,21 +3,20 @@ import CountDownPage from './pages/CountDown'
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      <Switch>
         <Route exact path='/'>
           <Home />
         </Route>
-
         <Route exact path='/countdown-page'>
           <CountDownPage />
         </Route>
-        <Route exact path='/prev'>
-          <div>hello word</div>
-        </Route>
+      </Switch>
     </Router>
     
   );
